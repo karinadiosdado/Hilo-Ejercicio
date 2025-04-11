c9aa3d322c623493ffd5ceee0c11873b2bcf1746
Karina Diosdado <l.dimk050419@itses.edu.mx>
2025-04-11 13:12:48 -0600

    el push de la fecha 10/04/2025 con avanze

--- src/main/java/mx/edu/itses/kldm/hilosejercicio/AlfanumericoTarea.java
+++ src/main/java/mx/edu/itses/kldm/hilosejercicio/AlfanumericoTarea.java
@@ -8,6 +8,41 @@ package mx.edu.itses.kldm.hilosejercicio;
  *
  * @author karin
  */
-public class AlfanumericoTarea {
+public class AlfanumericoTarea implements Runnable{
+
+    
+    public enum Tipo{
+        NUMERO,LETRA
+    }
+    
+    private Tipo tipo;
+
+    public AlfanumericoTarea(Tipo tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+    @Override
+    public void run() {
+        try {
+            if(tipo==tipo.NUMERO){
+                System.out.println("Hilo"+ Thread.currentThread().getName());
+                for (int i = 1; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + i);        
+                    Thread.sleep(2000);
+                }
+            }else if(tipo == tipo.LETRA){
+                System.out.println("Hilo" + Thread.currentThread().getName());
+                for (char x = 'a'; x <='z'; x++) {
+                    
+                     System.out.println(Thread.currentThread().getName() + x);        
+                    Thread.sleep(2000); 
+                }
+            }
+        } catch (InterruptedException e) {
+            
+        }
+    }
     
 }
