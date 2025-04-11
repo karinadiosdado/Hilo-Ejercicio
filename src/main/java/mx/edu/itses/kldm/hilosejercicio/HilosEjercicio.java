d6a2ff87670b516184c1aa1eb6ed299a1f4406a6
Karina Diosdado <l.dimk050419@itses.edu.mx>
2025-04-11 09:26:32 -0600

    el push de la fecha 10/04/2025

---
+++ src/main/java/mx/edu/itses/kldm/hilosejercicio/HilosEjercicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itses.kldm.hilosejercicio;
+
+/**
+ *
+ * @author karin
+ */
+public class HilosEjercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
